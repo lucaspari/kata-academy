2ae2b5c3e0a3108f9f025d249a988d486647c01c
Lucas Pari <lucas.pari@icloud.com>
2023-04-28 16:59:54 -0300

    feat: add login button pre-logic

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { auth } from "@/firebase/config";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 export default function Login() {
+  const signIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result);
+    });
+  };
   return (
-    <button className="text-white bg-gradient-to-r from-black to-purple-900 rounded-md py-2 px-4">
+    <button
+      onClick={() => signIn()}
+      className="text-white bg-gradient-to-r from-black to-purple-900 rounded-md py-2 px-4"
+    >
       Entrar
     </button>
   );
