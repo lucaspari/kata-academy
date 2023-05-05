2a26a1e3410fced057ea9dde26dba7ce8f91ddd2
lucaspari <lucaspariii@gmail.com>
2023-05-05 15:14:04 -0300

    feat: setting up all env variables to work with the workflow

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: "kata-acad.firebaseapp.com",
-  projectId: "kata-acad",
-  storageBucket: "kata-acad.appspot.com",
-  messagingSenderId: "927812031720",
-  appId: "1:927812031720:web:85619f49bd9252b9a0e55d",
-  measurementId: "G-78DQ0E0X3D",
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
