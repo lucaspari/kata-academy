641d262dd86bd4d482a3b2b01dcc8987ea7df4e7
lucaspari <lucaspariii@gmail.com>
2023-05-05 11:48:18 -0300

    feat: make some tests for home page

---
+++ src/__tests__/home/home.test.tsx
@@ -0,0 +1,21 @@
+import Home from "@/app/page";
+import "@testing-library/jest-dom"
+import {fireEvent, render,screen,waitFor} from '@testing-library/react'
+
+describe("Home.tsx",() =>{
+    it("test_render_home_without_errors", () => {
+        render(<Home />);
+        expect(screen.getByRole('main')).toBeInTheDocument();
+    });
+    it("renders texts from home",() =>{
+        const {getByText} = render(<Home/>)
+       expect(getByText("Aqui, você poderá praticar de forma segura e conveniente, tudo isso sem sair de casa.")).toBeInTheDocument()
+       expect(screen.getByRole('button', { name: /Aprenda Mais/i })).toBeInTheDocument();
+    })
+    it("test_button_click_event", () => {
+        const handleClick = jest.fn();
+        render(<button onClick={handleClick}>Aprenda Mais</button>);
+        fireEvent.click(screen.getByRole('button', { name: /Aprenda Mais/i }));
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    });   
+})
