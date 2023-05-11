b3fc20e73181c4b1f5e4e8e254ec2fda50899177
Lucas Pari <lucas.pari@icloud.com>
2023-05-11 17:40:43 -0300

    feat: added contextAPI and implemented login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { auth } from "@/firebase/config";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+import { User, UserContext } from "@/context/userContext";
+import { useContext } from "react";
 export default function Login() {
+  const userContext = useContext(UserContext)
+  if (!userContext) {
+    throw new Error('User context is null or undefined');
+  }
   const signIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider).then((result) => {
-      console.log(result);
+      const user : User ={
+        displayName : result.user.displayName ?? "",
+        email : result.user.email ?? "",
+        photoURL:result.user.photoURL ?? "",
+      }
+      userContext.updateUser(user)     
     });
   };
   return (
---
+++ src/context/userContext.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+import React from 'react'
+
+interface UserProviderProps{
+  children : React.ReactNode
+}
+export interface User {
+  displayName: string;
+  email: string;
+  photoURL: string;
+}
+
+type UserContextType = {
+  user: User | undefined;
+  saveUser: (user: User) => void;
+  updateUser: (user: User) => void;
+};
+
+export const UserContext = createContext<UserContextType | null>(null);
+export const UserProvider: React.FC<UserProviderProps> = ({children}) => {
+    const [user, setUser] = useState<User | undefined>();
+    const saveUser = (user: User) => {
+      setUser(user);
+    };
+  
+    const updateUser = (updatedUser: User) => {
+      setUser((prevUser) => ({
+        ...prevUser,
+        ...updatedUser,
+      }));
+    };
+    return (
+      <UserContext.Provider value={{ user, saveUser, updateUser }}>
+        {children}
+      </UserContext.Provider>
+    );
+  };
+export default UserProvider;
