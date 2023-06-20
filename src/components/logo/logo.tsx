84fdf947a2a550e7c25650ef901aaf0b35263ba1
lucaspari <lucaspariii@gmail.com>
2023-06-20 13:15:13 -0300

    chore: changing logo image to tsx component

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,20 +1,11 @@
-import logo_black from "@/public/logo_black.svg";
-import logo_white from "@/public/logo_white.svg";
-import Image from "next/image";
+import { LogoBlack } from "../logoBlack/logoBlack";
+import { LogoWhite } from "../logoWhite/logoWhite";
 
 export default function Logo() {
   return (
     <div>
-      <Image
-        className="hidden dark:inline-block"
-        src={logo_black}
-        alt="logo"
-      ></Image>
-      <Image
-        className="inline-block dark:hidden"
-        src={logo_white}
-        alt="logo"
-      ></Image>
+      <LogoBlack />
+      <LogoWhite />
     </div>
   );
 }
