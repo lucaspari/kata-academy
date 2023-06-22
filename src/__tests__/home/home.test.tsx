97497f1c9d8ab2a93a41a8b12c8fc6c784b90fce
lucaspari <lucaspariii@gmail.com>
2023-06-22 17:15:54 -0300

    fix: fixing test home

--- src/__tests__/home/home.test.tsx
+++ src/__tests__/home/home.test.tsx
@@ -1,8 +1,19 @@
 import Home from "@/app/page";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+jest.mock("next-auth/react");
+
 describe("Home.tsx", () => {
-  it("home exists?", () => {
+  it("home exists?", async () => {
+    useSession.mockReturnValueOnce([
+      {
+        user: {
+          email: "foo@bar.com",
+        },
+      },
+      false,
+    ]);
     render(<Home />);
   });
 });
