1a98fbc82d4d5fdab096f15c7ac0226bc4a50aee
lucaspari <lucas.pari@icloud.com>
2023-10-03 11:23:20 -0300

    feat: modified display of text in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import SideBar from "@/components/sideBar/sideBar";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import chibi from "@/public/karate-chibi.jpg";
+import "./style.css"
 export default function Dashboard() {
   const session = useSession();
   return session ? (
@@ -11,8 +12,8 @@ export default function Dashboard() {
       <SideBar />
       <main className="half-screen mt-8">
         <div className="flex flex-col items-center">
-        <Image alt="chibi" width={400} className="rounded-lg m-2 p-2" src={chibi}></Image>
-        <h1 className="flex justify-center text-3xl m-2 p-2">
+        <Image alt="chibi" width={400} className="rounded-[4.5rem] m-2 p-2" src={chibi}></Image>
+        <h1 className="flex justify-center text-3xl m-2 p-2 text-align-last-center">
           Obrigado por se juntar a nossa plataforma de karate,
           aqui você ira aprender técnicas disseminadas pelo karatedô shotokan,
           este sendo considerado um karate moderno
