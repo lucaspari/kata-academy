91efdca33b436e0f0e99a2b0cd75ead1d1581921
Lucas Fernando Pari <lucas.pari@icloud.com>
2023-11-01 15:41:42 -0300

    format: formating error.js

--- src/app/error.js
+++ src/app/error.js
@@ -1,27 +1,33 @@
-"use client"
+"use client";
 import SideBar from "@/components/sideBar/sideBar";
 import Image from "next/image";
-import Chibi_crying from "@/public/chibi_crying.jpg"
-export default function NotFound(){
-    return (
-        <div className='flex'>
-        <SideBar/>
-          <main className="flex-grow max-w-screen-lg mx-8">
-      <div className="w-1/2 flex flex-col items-center gap-8">
-            <div className="text-5xl text-center my-4">
-              Infelizmente essa página não pode ser carregada, por gentileza.
-              Contate o administrador
-            </div>
-            <Image src={Chibi_crying} className="rounded-lg"  alt="Chibi_crying" width={400} height={400} />
-            <a
-      className="text-center text-2xl underline"
-              href="mailto:lucaspariii@gmail.com?Subject=Something wrong with application"
-              target="_blank"
-            >
-              Envie um email
-            </a>
-      </div>
-          </main>
+import Chibi_crying from "@/public/chibi_crying.jpg";
+export default function NotFound() {
+  return (
+    <div className="flex">
+      <SideBar />
+      <main className="flex-grow max-w-screen-lg mx-8">
+        <div className="w-1/2 flex flex-col items-center gap-8">
+          <div className="text-5xl text-center my-4">
+            Infelizmente essa página não pode ser carregada, por gentileza.
+            Contate o administrador
+          </div>
+          <Image
+            src={Chibi_crying}
+            className="rounded-lg"
+            alt="Chibi_crying"
+            width={400}
+            height={400}
+          />
+          <a
+            className="text-center text-2xl underline"
+            href="mailto:lucaspariii@gmail.com?Subject=Something wrong with application"
+            target="_blank"
+          >
+            Envie um email
+          </a>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
